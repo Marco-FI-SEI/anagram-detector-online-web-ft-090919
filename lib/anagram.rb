bf54170be134d45d3836ff5c3ace200b50486c11
Marco-FI-SEI <mmc90@hotmail.co.uk>
2020-04-15 21:29:57 +0100

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    anagrams = words.find_all do |w|
+      return w if w == w.split("").sort
+    end
+  end
+  anagrams
+end
